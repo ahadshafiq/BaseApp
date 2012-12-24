b425970e68a06680fc7c4dba2ee8f660baad0d22
ahadshafiq <ahad.shafiq@gmail.com>
2012-12-24 20:17:48 +0300

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  	describe "Home page" do
+  		it "should have the content 'Sample App'" do
+  			visit '/static_pages/home' #this is the part of the cabibara which works with rspec 
+  			page.should have_selector('h1', :text => 'Sample App')
+  		end
+
+  		it "should have the correct title tag" do
+  			visit '/static_pages/home' #this is the part of the cabibara which works with rspec 
+  			page.should have_selector('title', :text => "Twitter Sample App | Home")
+  		end
+   	 end
+
+	describe "Help page" do
+  		it "should have the content 'Help'" do
+  			visit '/static_pages/help'
+  			page.should have_selector('h1', :text =>'Help')
+  		end
+  		it "should have the correct title tag" do
+  			visit '/static_pages/help' #this is the part of the cabibara which works with rspec 
+  			page.should have_selector('title', :text => "Twitter Sample App | Help")
+  		end
+   	 end
+
+   	describe "About page" do
+  		it "should have the content 'About'" do
+  			visit '/static_pages/about'
+  			page.should have_selector('h1', :text =>'About')
+  		end
+  		it "should have the correct title tag" do
+  			visit '/static_pages/about' #this is the part of the cabibara which works with rspec 
+  			page.should have_selector('title', :text => "Twitter Sample App | About")
+  		end
+   	end
+
+   	describe "Contact page" do
+   		it "should have the h1 Contact" do
+   			visit '/static_pages/contact'
+   			page.should have_selector('h1', :text => 'Contact')
+   		end
+   		it "should have the correct title tag" do
+  			visit '/static_pages/contact' #this is the part of the cabibara which works with rspec 
+  			page.should have_selector('title', :text => "Twitter Sample App | Contact")
+  		end
+   	end
+
+
+end
+	
