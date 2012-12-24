c067ae83ef9258d88417fb26b5b794c334788ec5
ahadshafiq <ahad.shafiq@gmail.com>
2012-12-24 23:46:03 +0300

    Added the title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,10 +7,13 @@ describe "Static pages" do
   			visit '/static_pages/home' #this is the part of the cabibara which works with rspec 
   			page.should have_selector('h1', :text => 'Sample App')
   		end
-
-  		it "should have the correct title tag" do
+  		it "should have the correct base tag" do
   			visit '/static_pages/home' #this is the part of the cabibara which works with rspec 
-  			page.should have_selector('title', :text => "Twitter Sample App | Home")
+  			page.should have_selector('title', :text => "Twitter Sample App")
+  		end
+  		it "should not have a custom page title" do
+  			visit '/static_pages/home' #this is the part of the cabibara which works with rspec 
+  			page.should_not have_selector('title', :text => '| Home')
   		end
    	 end
 
