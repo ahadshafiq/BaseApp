92e6c2591dc9854c52c55e9188c809ef5143a5bb
ahadshafiq <ahad.shafiq@gmail.com>
2012-12-29 10:30:25 +0300

    Sign in, Sign out,  Sessions

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,29 @@
+module SessionsHelper
+	
+	def sign_in(user)
+		#sign the user in
+		#cookies[:remember_token] = { value: user.remember_token, expires: 20.years.from_now}
+		# or 
+		cookies.permanent[:remember_token] = user.remember_token
+		current_user = user
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+		# @current_user = @current_user || User.find_by_remember_token(cookies[:remember_token])
+	end
+
+	def sign_out
+		current_user = nil
+		cookies.delete(:remember_token)
+	end
+
+end
